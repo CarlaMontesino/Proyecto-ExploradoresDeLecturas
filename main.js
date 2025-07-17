e62c5c19ff10fd47d6d1b3d56dd06f9a6a5c5032
Carla M <carlafernandamontesino@gmail.com>
2025-07-17 02:06:47 -0300

    prueba img cuento json

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ function cargarJSON(url, idGaleria) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      data.forEach(item => {
+      data.forEach(cuento => {
         const card = document.createElement('div');
         card.className = 'tarjeta-cuento';
         card.innerHTML = `
-          <img src="${item.imagen}" alt="Imagen de ${item.titulo}">
-          <h4>${item.titulo}</h4>
-          <p><strong>Autor:</strong> ${item.autor}</p>
-          <p>${item.descripcion}</p>
+          <img src="${cuento.imagen}" alt="Portada de ${cuento.titulo}">
+          <h4>${cuento.titulo}</h4>
+          <p><strong>Autor:</strong> ${cuento.autor}</p>
+          <p>${cuento.descripcion}</p>
         `;
         galeria.appendChild(card);
       });
